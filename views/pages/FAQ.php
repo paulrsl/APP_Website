19d25fae59c7088aa0c4a5f7e45de50fa118ffb4
paulrsl <59578157+paulrsl@users.noreply.github.com>
2020-04-25 23:50:06 +0200

    FAQ graphique interface

--- views/pages/FAQ.php
+++ views/pages/FAQ.php
@@ -12,55 +12,56 @@ if(isset($_SESSION["userTypeAccess"])){?>
 
     <?php if(($_SESSION["userTypeAccess"] == "user") || ($_SESSION["userTypeAccess"] == "organism") || ($_SESSION["userTypeAccess"] == "admin")){?>
 
-        <ul>
+        <ul class="faq-list">
         <?php
         $faq = getFAQ($_SESSION["userLanguage"])->fetchAll();
         foreach ($faq as $ans){
             ?>
-            <li>
+            <li class="faq-item">
                 <div>
-                    <a><?= $ans["question"]; ?></a>
+                    <a class="faq-question"><?= $ans["question"]; ?></a>
 
                     <?php if($_SESSION["userTypeAccess"] == "admin"){?>
 
-                        <a><?php echo _MODIFY ?></a>
-                        <a><?php echo _DELETE ?></a>
+                        <input class="smallButton" type="submit" value="<?php echo _MODIFY ?>">
+                        <input class="smallButton" type="submit" value="<?php echo _DELETE ?>">
 
                     <?php }?>
                     <br>
-                    <a><?= $ans["answer"]; ?></a><br><br>
+                    <a class="faq-answer"><?= $ans["answer"]; ?><br></a>
                 </div>
             </li>
 
         <?php } ?>
         </ul>
+        <br>
         <?php if($_SESSION["userTypeAccess"] == "admin"){?>
 
-            <a><?php echo _ADDMODIFY ?></a>
+            <?php echo _ADDQUESTION ?>
             <form method="post" action="index.php?action=addFAQ" enctype="multipart/form-data">
 
                 <p>
-                    <label><?php echo _QUESTION;?>
+                    <label><?php echo _QUESTION;?><br>
                         <textarea name="question" rows="2" cols="50"></textarea>
                     </label>
                 </p>
 
                 <p>
-                    <label><?php echo _ANSWER;?>
+                    <label><?php echo _ANSWER;?><br>
                         <textarea name="answer" rows="2" cols="50"></textarea>
                     </label>
                 </p>
 
                 <p>
-                    <a><?php echo _LANGUAGE;?> ?</a>
+                    <?php echo _LANGUAGE;?> ?
                     <br>
-                    <select type = 'text' name="language" required>
+                    <select id="languageSelect" type='text' name="language" required>
                         <option value="EN" id="user">EN</option>
                         <option value="FR" id="organism">FR</option>
                     </select>
                 </p>
 
-                <input type="submit" value="<?php echo _SUBMIT;?>" >
+                <input class="bigButton" type="submit" value="<?php echo _SUBMIT;?>" >
 
             </form>
 
