783e00a2353d82fa1499fc1dcbc2e7457c39ba9c
paulrsl <59578157+paulrsl@users.noreply.github.com>
2020-04-25 21:36:00 +0200

    Revert "Ré-organisation interface graphique (sur 2 feuilles de style)"
    This reverts commit 290b56a3f47585e752a93b0216b46d8cd75ddd38.

--- controllers/language.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    function language($language){
-        switch($language){
-            case "EN" : $_SESSION["language"] = "EN"; break;
-            case "FR" : $_SESSION["language"] = "FR"; break;
-        }
-    }
---
+++ views/forgotPassword.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="utf-8"/>
+    <title>BIG-MAP Forgot password</title>
+    <link rel="icon" type="image/png" href="/APP_Website/pictures/Logo BIG-MAP.png"/>
+    <link rel="stylesheet" href="/APP_Website/design/css/forgotPasswordCSS.css "/>
+    <link rel="stylesheet" href="/APP_Website/design/JavaScript/forgotPasswordJS.js "/>
+</head>
+
+<body>
+
+<p>What's your e-mail address ?</p>
+
+<p>
+    <label>Mail :<br>
+        <input type='text' name="mail" placeholder="Mail" required>
+    </label>
+</p>
+
+<input type="submit" value="Send new password"></br></br>
+
+<a href="index.php?page=connection">Go back</a></br>
+
+<script src="/APP_Website/design/JavaScript/forgotPasswordJS.js"></script>
+
+</body>
+
+</html>
