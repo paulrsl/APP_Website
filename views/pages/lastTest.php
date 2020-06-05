58932348e4350ece146a786ce183498845584618
Maximilien Teil <maximilien.teil@gmail.com>
2020-06-05 15:19:50 +0200

    Version finale du site

---
+++ views/pages/lastTest.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+<?php include "views/templates/main-nav.php"; ?>
+<?php include "views/templates/headerLogin.php"; ?>
+
+<div class="page">
+    <div class="container">
+        <?php
+        if(isset($_SESSION["userTypeAccess"])){?>
+            <h2 class="subpage"><?php echo _LASTTEST;?></h2>
+
+            <?php if($_SESSION["userTypeAccess"] == "user"){?>
+                <table id="resultTable">
+                    <tr>
+                        <th><?php echo _FIRSTNAME;?></th>
+                        <th><?php echo _LASTNAME;?></th>
+                        <th><?php echo _DATE;?></th>
+                        <th><?php echo _AVERAGEVISUALSTIMULUS;?></th>
+                        <th><?php echo _AVERAGESOUNDSTIMULUS;?></th>
+                        <th><?php echo _AVERAGETONE;?></th>
+                    </tr>
+
+                    <?php $userId = getUserId($_SESSION["userId"])->fetchAll();
+                    $results = getResults($userId[0][0])->fetchAll();
+
+                    foreach ($results as $result) { ?>
+                        <tr>
+                            <td>
+                                <?php $personId =  getPersonId($result['userId'])->fetchAll();
+                                $info = getInfos((int) $personId[0][0])->fetchAll();
+                                $info = $info[0];
+                                echo $info["firstName"]?>
+                            </td>
+                            <td>
+                                <?php echo $info["lastName"]?>
+                            </td>
+                            <td>
+                                <?php echo $result["testDate"];?>
+                            </td>
+                            <td>
+                                <?php echo $result["averageVisualStimulus"];?>
+                            </td>
+                            <td>
+                                <?php echo $result["averageSoundStimulus"];?>
+                            </td>
+                            <td>
+                                <?php echo $result["averageTone"];?>
+                            </td>
+                        </tr>
+                    <?php }?>
+                </table>
+
+            <?php }elseif($_SESSION["userTypeAccess"] == "organism"){?>
+                <table id="resultTable">
+                    <tr>
+                        <th><?php echo _FIRSTNAME;?></th>
+                        <th><?php echo _LASTNAME;?></th>
+                        <th><?php echo _DATE;?></th>
+                        <th><?php echo _AVERAGEVISUALSTIMULUS;?></th>
+                        <th><?php echo _AVERAGESOUNDSTIMULUS;?></th>
+                        <th><?php echo _AVERAGETONE;?></th>
+                    </tr>
+
+                    <?php $id = getOrganismId($_SESSION["userId"])->fetchAll();
+                    if(empty($id) == false){ ?>
+                        <?php $listId = getUserInOrganism((int) $id[0][0]);
+                        $resultArray=array();
+                        foreach ($listId as $list){
+
+                            $results = getResults((int) $list [0])->fetchAll();
+                            foreach ($results as $result){
+
+                                array_push($resultArray,$result);
+                            } }
+
+                        function cmp($a, $b){
+                            return $a["testDate"] < $b["testDate"];
+                        }
+
+                        usort($resultArray, "cmp");
+
+                        $i=0;
+                        while ($i < sizeof($resultArray)){
+                            $personId =  getPersonId($resultArray[$i][1])->fetchAll();
+                            $info = getInfos((int) $personId[0][0])->fetchAll();
+                            $info = $info[0]; ?>
+
+                                <tr>
+                                    <td>
+                                        <?php echo $info['firstName']?>
+                                    </td>
+                                    <td>
+                                        <?php echo $info['lastName']?>
+                                    </td>
+                                    <td>
+                                        <?php echo $resultArray[$i][2]?>
+                                    </td>
+                                    <td>
+                                        <?php echo $resultArray[$i][6];?>
+                                    </td>
+                                    <td>
+                                        <?php echo $resultArray[$i][7];?>
+                                    </td>
+                                    <td>
+                                        <?php echo $resultArray[$i][8];?>
+                                    </td>
+                                </tr>
+                            <?php $i+=1;?>
+                            <?php }?>
+
+                    <?php }else{ echo _NOMEMBER; } ?>
+                </table>
+
+            <?php }elseif($_SESSION["userTypeAccess"] == "admin"){?>
+
+            <?php }?>
+
+        <?php }else{ ?>
+
+        <?php } ?>
+    </div>
+</div>
+
+</div> <!--fin du bloc main-->
+
+</body>
+
+</html>
+
